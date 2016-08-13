c80c3a0e87251c4ed1b56afd6965293d7ed6d486
Tung Hoang <thoang3@uic.edu>
2016-08-12 19:22:06 -0500

    add

---
+++ StudentInformation/StudentInformationTester.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class StudentInformationTester {
+	
+	
+	StudentInformation studentInfo = new StudentInformation("SampleData.csv");
+ 
+    
+    @Test
+	public void test() {
+    	
+    	assertEquals(100, 10 * 10);
+		
+	}
+    
+}
