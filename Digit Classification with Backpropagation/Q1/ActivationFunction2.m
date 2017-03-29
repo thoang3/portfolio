4dce14ab830b16e303204f0b4983c444713294e1
Tung Hoang <thoang3@uic.edu>
2017-03-29 18:59:37 -0500

    Added Digit Classify

---
+++ Digit Classification with Backpropagation/Q1/ActivationFunction2.m
@@ -0,0 +1,4 @@
+%% Activation Function 2
+function phi2 = ActivationFunction2(v) 
+        phi2 = 1/10 * v; 
+end
---
+++ Digit Classification with Backpropagation/Q2/ActivationFunction2Deri.m
@@ -0,0 +1,5 @@
+%% Activation Function 1 Derivative
+function phi2Deri = ActivationFunction2Deri(v) 
+        phi2Deri = 1/10; 
+        %phi2Deri = 1/5;
+end
