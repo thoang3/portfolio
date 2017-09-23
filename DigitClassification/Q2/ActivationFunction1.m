f268fdc26cb112eafa63161314c2cf13ab7a2e47
htt1084 <thoang3@uic.edu>
2017-09-23 18:30:05 -0500

    tung

--- DigitClassification/Q2/ActivationFunction1.m
+++
@@ -1,5 +0,0 @@
-%% Activation Function 1
-function phi1 = ActivationFunction1(v) 
-        phi1 = -1 + 2./(1 + exp(-2*v)); 
-        %phi1 = 1./(1+exp(-v));
-end
--- MineSweeper/HeadPictureStates.java
+++
@@ -1,8 +0,0 @@
-
-public enum HeadPictureStates {
-	HEAD_DEAD, 
-	HEAD_GLASSES, 
-	HEAD_O, 
-	HEAD_SMILE, 
-	HEAD_SMILE_PRESSED
-}
